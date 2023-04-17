34d781424ccda08e582dd373548cc49e7160dee5
Gibran Abdillah <unkownuser145@gmail.com>
2023-04-17 21:55:56 +0700

    Update cron.py

--- newsaggregator/cron.py
+++ newsaggregator/cron.py
@@ -1,11 +1,15 @@
 import schedule, os , time
 
 
+print("do something")
+
 def task():
+    print("running the task...")
     os.system("python3 manage.py scrape")
 
-schedule.every(10).minutes.do(task)
+schedule.every(1).minutes.do(task)
 
 while True:
+    print("pending task...")
     schedule.run_pending()
     time.sleep(1)
